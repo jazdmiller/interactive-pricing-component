d0b6701a7a45eaaee190d463ff9bc6d2587dea73
Jazmine Miller <jazminedmiller@gmail.com>
2023-09-23 14:51:03 -0400

    add pattern-circles svg

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import PatternCircles from '../images/pattern-circles.svg'
+
 
 function Home() {
   return (
     <div>
-
-    
         <div className='bg-img'>        
-
+            <img src={PatternCircles} />
         </div>
     </div>
   )
