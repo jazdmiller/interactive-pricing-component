09b266c8ed2a647a80e7863399ab647a7afc62c7
Jazmine Miller <jazminedmiller@gmail.com>
2023-09-24 15:49:13 -0400

    style header text

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,18 @@ function Home() {
   return (
     <div>
         <div className='bg-img text-center pt-5'>       
-            <div className=' bg-pattern bold-header-text row align-items-center '>
-                <div>
+            <div className=' bg-pattern row align-items-center '>
+                <div className='bold-header'>
                 Simple, traffic-based pricing
                 </div>
                 <div>
-                Sign-up for our 30-day trial. No credit card required. 
+                Sign-up for our 30-day trial.<br className='d-lg-none' /> No credit card required. 
                 </div>
+            </div>
+        </div>
+        <div className='card w-50 mx-auto'>
+            <div className='card-body'>
+
             </div>
         </div>
     </div>
