0ecf2e20f1080c70742df9a697aaaf0cb20fc866
Jazmine Miller <jazminedmiller@gmail.com>
2023-09-26 12:45:28 -0400

    add divider below toggle section

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,8 +5,8 @@ function Card() {
     <div>
          <div className='card w-md-50 mx-auto'>
             <div className='card-body p-4'>
-                <div className='row '>
-                    <div className='col-md-6 order-sm-1 text-center text-md-start col-margin' id='pageview-text'>
+                <div className='row mb-3'>
+                    <div className='col-md-6 order-sm-1 pt-md-4 text-center text-md-start col-margin' id='pageview-text'>
                         100k PAGEVIEWS
                     </div>
                     <div className='col-md-6 order-3 order-md-2 text-center text-md-end col-margin'>
@@ -17,7 +17,7 @@ function Card() {
                 </div>
                 </div>
 
-                <div className='row billing-row col-margin justify-content-center' id="billing-row">
+                <div className='row billing-row justify-content-center' id="billing-row">
                     <div className='col-4 px-0 text-end'>Monthly billing</div>
                     <div className='col-4 px-2 text-center form-check form-switch'>
                         <input className='form-check-input' type='checkbox' role='switch' id='flexSwitchCheckDefault'/>
